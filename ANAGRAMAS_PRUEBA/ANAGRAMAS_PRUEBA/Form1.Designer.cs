76c1ea57801760ebaea6e7deca2e87dd91b7ea44
ejocobi <105555761+ejocobi@users.noreply.github.com>
2022-05-14 09:26:38 -0600

    Add files via upload

---
+++ ANAGRAMAS_PRUEBA/ANAGRAMAS_PRUEBA/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ANAGRAMAS_PRUEBA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalabra1 = new System.Windows.Forms.TextBox();
+            this.txtPalabra2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Palabra 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Palabra 2:";
+            // 
+            // txtPalabra1
+            // 
+            this.txtPalabra1.Location = new System.Drawing.Point(318, 106);
+            this.txtPalabra1.Name = "txtPalabra1";
+            this.txtPalabra1.Size = new System.Drawing.Size(100, 26);
+            this.txtPalabra1.TabIndex = 3;
+            // 
+            // txtPalabra2
+            // 
+            this.txtPalabra2.Location = new System.Drawing.Point(318, 183);
+            this.txtPalabra2.Name = "txtPalabra2";
+            this.txtPalabra2.Size = new System.Drawing.Size(100, 26);
+            this.txtPalabra2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 450);
+            this.Controls.Add(this.txtPalabra2);
+            this.Controls.Add(this.txtPalabra1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPalabra1;
+        private System.Windows.Forms.TextBox txtPalabra2;
+    }
+}
+
---
+++ ANAGRAMAS_PRUEBA/ANAGRAMAS_PRUEBA/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANAGRAMAS_PRUEBA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String palabra = txtPalabra1.Text;
+            String palabra2 = txtPalabra2.Text;
+
+            var resultado = SonAnagramas(palabra.ToLower(), palabra2.ToLower());
+
+
+            if (resultado == true)
+            {
+                MessageBox.Show("SON ANAGRAMA", "PRUEBA DE MENSAJE DESDE LA APP", MessageBoxButtons.OK);
+            }
+            else {
+                MessageBox.Show("NO SON ANAGRAMA", "PRUEBA DE MENSAJE DESDE LA APP", MessageBoxButtons.OK);
+            }
+        }
+
+        public static bool SonAnagramas(string palabra, string palabra2)
+        {
+            if (palabra.Length != palabra2.Length) return false;
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                bool igualdad = false;
+                for (int j = 0; j < palabra2.Length; j++)
+                {
+                    if (palabra[i] == palabra2[j])
+                    {
+                        palabra2 = palabra2.Remove(j, 1);
+                        igualdad = true;
+                        break;
+                    }
+                }
+                if (!igualdad) return false;
+            }
+            return palabra2.Length == 0;
+        }
+    }
+}
